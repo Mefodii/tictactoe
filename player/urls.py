be3638517d03c7512b144952342c1f966427cdf9
Mefodii <mefodii04@gmail.com>
2018-03-14 17:04:49 +0200

    invitations - alpha

--- player/urls.py
+++ player/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import home
+from .views import home, new_invitation
 
 urlpatterns = [
     path('home', home, name="player_home"),
     path("login", LoginView.as_view(template_name="player/login_form.html"),
          name="player_login"),
     path("logout", LogoutView.as_view(),
-         name="player_logout")
+         name="player_logout"),
+    path("new_invitation", new_invitation,
+         name="player_new_invitation")
 
 ]
